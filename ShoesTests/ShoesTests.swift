f3a697acffeb097aeaaf2605be3bda16076c0358
Victor Frolov <vicfrolov@gmail.com>
2015-12-28 22:07:05 -0800

    Initial Commit

---
+++ ShoesTests/ShoesTests.swift
@@ -0,0 +1,36 @@
+//
+//  ShoesTests.swift
+//  ShoesTests
+//
+//  Created by Victor Frolov on 12/28/15.
+//  Copyright © 2015 Victor Frolov. All rights reserved.
+//
+
+import XCTest
+@testable import Shoes
+
+class ShoesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
