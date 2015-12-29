ea7157f038ffbc2387cb1fde2e7d5471bf65848d
Victor Frolov <vicfrolov@gmail.com>
2015-12-28 23:25:24 -0800

    added catered images and links

---
+++ Shoes/CuratedSearchTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  CuratedSearchTableViewCell.swift
+//  Shoes
+//
+//  Created by Victor Frolov on 12/28/15.
+//  Copyright © 2015 Victor Frolov. All rights reserved.
+//
+
+import UIKit
+
+class CuratedSearchTableViewCell: UITableViewCell {
+    
+    @IBOutlet var cityLabel: UILabel!
+    @IBOutlet var logLabel: UILabel!
+    @IBOutlet var thumbnailImageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
