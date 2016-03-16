40efddeb81071b5aa5e7ac13e5e8eefb5ac7540a
Victor Frolov <vicfrolov@gmail.com>
2016-03-16 02:08:10 -0700

    added a loginPrompt VC, implemented methods and outlets for login and login and creation of account

---
+++ DataService.swift
@@ -0,0 +1,45 @@
+//
+//  DataService.swift
+//  PocketDistrict
+//
+//  Created by Victor Frolov on 3/16/16.
+//  Copyright © 2016 Victor Frolov. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class DataService {
+    static let dataService = DataService()
+    
+    private var _BASE_REF = Firebase(url: "\(BASE_URL)")
+    private var _USER_REF = Firebase(url: "\(BASE_URL)/users")
+    private var _JOKE_REF = Firebase(url: "\(BASE_URL)/jokes")
+    
+    var BASE_REF: Firebase {
+        return _BASE_REF
+    }
+    
+    var USER_REF: Firebase {
+        return _USER_REF
+    }
+    
+    var CURRENT_USER_REF: Firebase {
+        let userID = NSUserDefaults.standardUserDefaults().valueForKey("uid") as! String
+        
+        let currentUser = Firebase(url: "\(BASE_REF)").childByAppendingPath("users").childByAppendingPath(userID)
+        
+        return currentUser!
+    }
+    
+    var JOKE_REF: Firebase {
+        return _JOKE_REF
+    }
+    
+    func createNewAccount(uid: String, user: Dictionary<String, String>) {
+        
+        // A User is born.
+        
+        USER_REF.childByAppendingPath(uid).setValue(user)
+    }
+}
