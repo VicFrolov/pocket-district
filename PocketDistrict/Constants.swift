067135b53637417c72bbbfcdfe4bc7564ffc2628
Victor Frolov <vicfrolov@gmail.com>
2016-03-16 01:03:30 -0700

    added a constants file, restructured folders for MVC principles

---
+++ PocketDistrict/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  PocketDistrict
+//
+//  Created by Victor Frolov on 3/16/16.
+//  Copyright © 2016 Victor Frolov. All rights reserved.
+//
+
+import Foundation
+
+let BASE_URL = "https://pocketdistrict.firebaseio.com/";
+
+
+
+
+
