cb167f9760886fdad8130d60986bcf9aa5298c95
Victor Frolov <vicfrolov@gmail.com>
2016-04-05 01:58:42 -0700

    pins are now displaying basic info like username and userPost. There is a problem with too many requests to apples server, unfortunately so not all pins will show up

--- PocketDistrict/SearchResultsMapViewController.swift
+++ PocketDistrict/SearchResultsMapViewController.swift
@@ -14,30 +14,6 @@ class SearchResultsMapViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Convert address to coordinate and annotate it on map
-//        let geoCoder = CLGeocoder()
-//        geoCoder.geocodeAddressString("7130 W. 85th Street, Los Angeles, CA", completionHandler: {
-//            placemarks, error in
-//            if error != nil {
-//                print(error)
-//                return
-//            }
-//            if let placemarks = placemarks {
-//                // Get the first placemark
-//                let placemark = placemarks[0]
-//                // Add annotation
-//                let annotation = MKPointAnnotation()
-//                annotation.title = "test"
-//                annotation.subtitle = "test2"
-//                if let location = placemark.location {
-//                    annotation.coordinate = location.coordinate
-//                    // Display the annotation
-//                    self.mapView.showAnnotations([annotation], animated: true)
-//                    self.mapView.selectAnnotation(annotation, animated: true)
-//                }
-//            }
-//        })
         
     }
     
@@ -46,7 +22,6 @@ class SearchResultsMapViewController: UIViewController {
         
         //drop pins
         if let tbc = self.tabBarController as? CustomTabBarController {
-            
             for i in (0 ..< tbc.categorizedArray.count) {
                 
                 //make sure that the specified index actually has a geoLon and geoLat
@@ -61,7 +36,7 @@ class SearchResultsMapViewController: UIViewController {
                     CLGeocoder().reverseGeocodeLocation(location, completionHandler: {
                         placemarks, error in
                         if error != nil {
-                            print(error)
+                            print("geoCoder error probably too many requests \(error)")
                             return
                         }
                         
@@ -70,8 +45,8 @@ class SearchResultsMapViewController: UIViewController {
                             let placemark = placemarks[0]
                             // Add annotation
                             let annotation = MKPointAnnotation()
-                            annotation.title = "test"
-                            annotation.subtitle = "test2"
+                            annotation.title = tbc.categorizedArray[i]["screenname"] as? String
+                            annotation.subtitle = tbc.categorizedArray[i]["userPost"] as? String
                             if let location = placemark.location {
                                 annotation.coordinate = location.coordinate
                                 // Display the annotation
@@ -87,18 +62,6 @@ class SearchResultsMapViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
